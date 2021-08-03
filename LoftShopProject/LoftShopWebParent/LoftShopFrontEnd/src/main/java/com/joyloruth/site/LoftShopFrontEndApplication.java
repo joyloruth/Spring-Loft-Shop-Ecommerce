945255212806cfc138efc1307a6b807af09809bd
Joy LoRuth <joyloruth@gmail.com>
2021-08-02 22:43:12 -0500

    front end project

---
+++ LoftShopProject/LoftShopWebParent/LoftShopFrontEnd/src/main/java/com/joyloruth/site/LoftShopFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.joyloruth.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoftShopFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoftShopFrontEndApplication.class, args);
+	}
+
+}
---
+++ LoftShopProject/LoftShopWebParent/LoftShopFrontEnd/src/test/java/com/joyloruth/site/LoftShopFrontEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joyloruth.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoftShopFrontEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
